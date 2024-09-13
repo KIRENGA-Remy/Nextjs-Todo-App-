590072640b1ed2de7b2188a9a729fcce6cae18d9
GITOLI Remy Claudien <gitoliremy@gmail.com>
2024-09-13 20:31:33 +0200

    navigation added

--- components/HeaderTop.tsx
+++ components/HeaderTop.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function HeaderTop() {
   return (
-    <div>
-      Header top
+    <div className='flex justify-around py-6'>
+      <div className='font-bold text-2xl'>FOODIED</div>
+      <div className='flex flex-row gap-4'>
+        <p className='text-white font-semibold capitalize text-xl cursor-pointer'>Home</p>
+        <p className='text-white font-semibold capitalize text-xl cursor-pointer'>Menu</p>
+        <p className='text-white font-semibold capitalize text-xl cursor-pointer'>Orders</p>
+        <p className='text-white font-semibold capitalize text-xl cursor-pointer'>About</p>
+        <p className='text-white font-semibold capitalize text-xl cursor-pointer'>Contacts</p>
+        <button className='px-2 py-1 bg-white text-black rounded-sm'>Login</button>
+        <button className='px-2 py-1 bg-green-500 text-black rounded-sm'>Signup</button>
+      </div>
     </div>
   )
 }
