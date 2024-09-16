b825f69642821f2d530ed9d64cae28636cb8a984
GITOLI Remy Claudien <gitoliremy@gmail.com>
2024-09-16 21:00:59 +0200

    food one added

--- components/HeaderTop.tsx
+++ components/HeaderTop.tsx
@@ -5,13 +5,13 @@ function HeaderTop() {
     <div className='flex justify-around py-6'>
       <div className='font-bold text-2xl'>FOODIED</div>
       <div className='flex flex-row gap-4'>
-        <p className='text-black font-semibold capitalize text-xl cursor-pointer'>Home</p>
-        <p className='text-black font-semibold capitalize text-xl cursor-pointer'>Menu</p>
-        <p className='text-black font-semibold capitalize text-xl cursor-pointer'>Orders</p>
-        <p className='text-black font-semibold capitalize text-xl cursor-pointer'>About us</p>
-        <p className='text-black font-semibold capitalize text-xl cursor-pointer'>Contacts</p>
-        <button className='px-3 py-1 bg-white text-black rounded-sm font-semibold cursor-pointer'>Login</button>
-        <button className='px-3 py-1 bg-green-500 text-black rounded-sm font-semibold cursor-pointer'>Signup</button>
+        <p className='text-black font-semibold capitalize text-xl cursor-pointer hover:underline'>Home</p>
+        <p className='text-black font-semibold capitalize text-xl cursor-pointer hover:underline'>Menu</p>
+        <p className='text-black font-semibold capitalize text-xl cursor-pointer hover:underline'>Orders</p>
+        <p className='text-black font-semibold capitalize text-xl cursor-pointer hover:underline'>About us</p>
+        <p className='text-black font-semibold capitalize text-xl cursor-pointer hover:underline'>Contacts</p>
+        <button className='px-3 py-1 bg-white text-black rounded-md font-semibold cursor-pointer'>Login</button>
+        <button className='px-3 py-1 bg-green-500 text-black rounded-md font-semibold cursor-pointer'>Signup</button>
       </div>
     </div>
   )
