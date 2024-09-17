b7f104597bc3a0e529042b4ae390a5cda712d660
GITOLI Remy Claudien <gitoliremy@gmail.com>
2024-09-17 18:48:25 +0200

    edit todo added

--- app/addTodo/page.jsx
+++ app/addTodo/page.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function AddTodo() {
   return (
-    <div>
-      Add Todo for today
-    </div>
+    <form action="" className='flex flex-col gap-3'>
+      <input type="text" placeholder='Todo Title...' className='border border-slate-500 px-8 py-2' />
+      <input type="text" placeholder='Todo Description...' className='border border-slate-500 px-8 py-2' />
+      <button className='bg-green-600 font-bold text-white py-3 px-6 w-fit'>Add Todo</button>
+    </form>
   )
 }
 
---
+++ app/editTodo/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EditTodo() {
+  return (
+    <div>
+      Edit TODO
+    </div>
+  )
+}
+
+export default EditTodo
